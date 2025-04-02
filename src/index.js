97a2b40b0778580fe91987c08bf1c36d4fd6d112
Esha Patel <Patellesha11@gmail.com>
2025-04-02 16:55:41 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './store';
+import "./loader.css"
 
-const container = document.getElementById('root');
-
-    const root = createRoot(container);
-    root.render(
-        <Provider store={store}>
-            <App />
-        </Provider>
-    )
+const Loader = () => {
+  return (
+    <span className="loader"></span>
+  )
+}
