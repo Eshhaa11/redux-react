58f0899f8114b490170c1917f06bcccadd611e8f
Esha Patel <Patellesha11@gmail.com>
2025-04-02 17:11:35 +0300

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useGetPokemonByNameQuery } from './services/pokemon';
 
 function App() {
    const { data , isLoading, isError } = useGetPokemonByNameQuery("ditto")
-
+   
    if(isError) {
     return <p>Error laoding pokemon character</p>
    }
