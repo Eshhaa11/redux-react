a9f20711610730321bca59cdb5b4fabe6f75df44
Esha Patel <Patellesha11@gmail.com>
2025-04-02 16:52:30 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
 import './App.css'
 import Loader from './components/loader' ;
 import { useGetPokemonByNameQuery } from './services/pokemon';
+
+function App() {
+   const { data , isLoading, isError } = useGetPokemonByNameQuery("ditto")
+
+   if(isError) [
+    return <p>Error laoding pokemon character</p>
+   ]
+}
