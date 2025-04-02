fde797ef7e8ff52f7458c89c470e727ebe8735b4
Esha Patel <Patellesha11@gmail.com>
2025-04-02 16:54:10 +0300

    added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import { useGetPokemonByNameQuery } from './services/pokemon';
 function App() {
    const { data , isLoading, isError } = useGetPokemonByNameQuery("ditto")
 
-   if(isError) [
+   if(isError) {
     return <p>Error laoding pokemon character</p>
-   ]
+   }
+
+   if (isLoading) {
+    return <Loader />
+   }
+
+   return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>REDUX</h1>
+      </header>
+    </div>
+   )
 }
